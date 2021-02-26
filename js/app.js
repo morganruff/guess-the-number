86445a0286289cc8c2d3f0955210f7b0f65c915b
morganruff <morgan.ruff@duke.edu>
2021-02-26 10:49:12 -0500

    update 1

--- js/app.js
+++ js/app.js
@@ -1,9 +1,39 @@
-/* --------- Constants ----------- */
+/*-------------------------------- Constants --------------------------------*/
 
-/* --------- Variables ----------- */
+/*-------------------------------- Variables --------------------------------*/
+let secretNum, guessList, isWinner;
 
-/* --------- Cached Element References  ----------- */
+/*------------------------ Cached Element References ------------------------*/
+const messageEl = document.getElementById('message');
+const guessesEl = document.getElementById('prevGuesses');
+const guessBtn = document.getElementById('guessButton');
+const resetBtn = document.getElementById('resetButton');
+const guessInput = document.getElementById('guessInput');
 
-/* --------- Event Listeners ----------- */
+/*----------------------------- Event Listeners -----------------------------*/
 
-/* --------- Functions  ----------- */
+resetBtn.addEventListener('click', function() {
+  init();
+});
+
+guessBtn.addEventListener('click', function(){
+    if (guessList.length === 0){
+        guessesEl.innerText = 'Previouus Guesses:'
+    }
+    if (isWinnner === false) {
+        checkGuess(parseInt(guessInput.value));
+    }
+})
+
+/*-------------------------------- Functions --------------------------------*/
+
+init();
+
+function init() {
+  guessesEl.innerText = '';
+  messageEl.innerText = 'Please enter a number between 1 and 100'
+  guessInput.value = '';
+  guessList = [];
+  isWinner = false;
+  secretNum = Math.floor(Math.random()*100) + 1;
+}
